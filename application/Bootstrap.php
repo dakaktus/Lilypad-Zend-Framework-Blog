4cf7b8e5d41c8c7d7ccafca82e88775929c0d72e
Kaktus <k.witkowski@advox.pl>
2014-01-18 14:27:58 +0100

    Added routes to bootstrap

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,21 +3,16 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
-    protected function _initDoctype() {
-        $this->bootstrap('view');
-        $view = $this->getResource('view');
-        $view->doctype('HTML5');
-        $view->headMeta()->appendHttpEquiv('Content-Type', 'text/html;charset=utf-8');
-    }
 
     protected function _initAutoload()
     {
         $this->options = $this->getOptions();
-        Zend_Registry::set('config.recaptcha', $this->options['recaptcha']);
-        Zend_Registry::set('config.message', $this->options['message']);
+//        Zend_Debug::dump($this->options);exit;
+//        Zend_Registry::set('config.recaptcha', $this->options['recaptcha']);
+//        Zend_Registry::set('config.message', $this->options['message']);
 
-        $db = Zend_Db::factory('PDO_MYSQL', $this->options['db']);
-        Zend_Db_Table_Abstract::setDefaultAdapter($db);
+//        $db = Zend_Db::factory('PDO_MYSQL', $this->options['db']);
+//        Zend_Db_Table_Abstract::setDefaultAdapter($db);
 
         $resourceLoader = new Zend_Application_Module_Autoloader (array(
           'basePath'  => APPLICATION_PATH . '/modules/admin',
@@ -46,21 +41,28 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $router = $front->getRouter();
 
         $config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/application.ini', 'production');
-        $router->addConfig($config,'routes');
 
-//        $tag_route = new Zend_Controller_Router_Route_Regex(
-//            'tag\/(.*)',
-//             array(
-//                 'module' => 'application',
-//                 'controller' => 'blog',
-//                 'action'     => 'tag'
-//             ),
-//             array(
-//                 'tag' => 1 // maps first subpattern "(*)" to "tag" parameter
-//             )
-//        );
-//
-//        $router->addRoute('tag', $tag_route);
+        $routes = array();
+
+        $routes['home'] = new Zend_Controller_Router_Route(
+            '/',
+            array(
+                'module' => 'defualt',
+                'controller' => 'index',
+                'action' => 'index'
+            )
+        );
+
+        $routes['blog'] = new Zend_Controller_Router_Route(
+            '/blog',
+            array(
+                'module' => 'defualt',
+                'controller' => 'blog',
+                'action' => 'index'
+            )
+        );
+
+        $router->addRoutes($routes);
 
         $blog_route = new Zend_Controller_Router_Route_Regex(
             '[0-9a-z\._!;,\+\-%]+-(\d+)', // all possible single entry URLs
@@ -78,18 +80,18 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     }
 
     protected function _initFooterLinks() {
-        $this->bootstrap('view');
-        $view = $this->getResource('view');
-        $tablegateway = new Zend_Db_Table('entries');
-        $blog_mapper = new Application_Model_EntryMapper($tablegateway);
-        $view->footer_blogs = $blog_mapper->findAll('4');
+//        $this->bootstrap('view');
+//        $view = $this->getResource('view');
+//        $tablegateway = new Zend_Db_Table('entries');
+//        $blog_mapper = new Application_Model_EntryMapper($tablegateway);
+//        $view->footer_blogs = $blog_mapper->findAll('4');
     }
 
      protected function _initModuleLangArray()
      {
          //Now let's define our language array to the registry so that we can use it...
          //Notice I have added "Application_" prefix to the registry to avoid any conflicts with other modules...
-         Zend_Registry::set("Application_language", $this->_options[$this->options["language"]["selected"]]);
+//         Zend_Registry::set("Application_language", $this->_options[$this->options["language"]["selected"]]);
      }
 }
 
